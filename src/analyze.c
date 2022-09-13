8c6f111e3a8cbfe44c621a407bffba144f1957b5
Jakob Kenda <kenda.jakob@gmail.com>
2022-09-13 20:12:01 +0200

    polepšan program za analizo

--- src/analyze.c
+++ src/analyze.c
@@ -1,9 +1,81 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <math.h>
+#include <limits.h>
 
-#define MAX_SAMPLES 250000
+#define MAX_SAMPLES 1000000 // milijon
 
-size_t frekvenca[256];
+
+size_t frekvenca[UINT8_MAX];
+int prev_bars = -1;
+int prev_percent = -1;
+int prev_width = -1;
+
+void print_progress(float progress)
+{
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    int window_width = w.ws_col;
+
+    int width   = window_width - 9;
+    int bars    = fmax(round(progress * width), 0);
+    int spaces  = width - bars;
+    int percent = round(progress * 100);
+    
+    if (bars != prev_bars || percent != prev_percent || window_width != prev_width)
+    {
+        printf("\r[");
+        for (int i = 0; i < bars; i++) putchar('=');
+        
+        if (bars > 0 && bars < width) putchar('>');
+        else if (bars == width) putchar('=');
+        else putchar(' ');
+        
+        for (int i = 0; i < spaces; i++) putchar(' ');
+        printf("] %3d %%", percent);
+        fflush(stdout);
+    }
+    
+    prev_bars = bars;
+    prev_percent = percent;
+    prev_width = window_width;
+}
+
+float a, b;
+uint32_t number;
+int offset = 0;
+int i_num = 0;
+size_t count = 0;
+size_t ncoords = 0;
+
+void calc_pi(uint8_t rand_byte)
+{
+    // iz 8-bitnih števil sestavi 32-bitno
+    number = (number << offset) | rand_byte;
+    
+    if (offset == 24) 
+    {
+        if (i_num == 0)
+            a = number / (float) UINT32_MAX;
+        else
+            b = number / (float) UINT32_MAX;
+        
+        if (i_num == 1)
+        {
+            // je znotraj kroga?
+            if (a*a + b*b < 1.0f) {
+                count++;
+            }
+            ncoords++;
+        }
+        
+        i_num = (i_num + 1) % 2;
+    }
+    
+    offset = (offset + 8) % 32;
+}
 
 int main()
 {
@@ -13,10 +85,11 @@ int main()
 
     for (int i = 0; i < MAX_SAMPLES; i++)
     {
-        uint8_t byte = getchar();
+        int byte = getchar();
         if (byte == EOF) break;
 
-        printf("\r%d %%", i * 100 / MAX_SAMPLES);
+        float progress = i / (float) MAX_SAMPLES;
+        print_progress(progress);
 
         for (int j = 0; j < 8; j++)
         {
@@ -26,25 +99,33 @@ int main()
             else if (bit == 1)
                 enic++;
         }
+        
+        calc_pi(byte);
 
         vsota += byte;
         samples++;
         frekvenca[byte]++;
     }
-    putchar('\n');
+
+    fclose(stdin);
+
+    double pi = (double) count / ncoords * 4;
 
     printf("E(bit) = %lf, ", enic / (double) (nicel + enic));
-    printf("E(byte) = %lf\n", vsota / (double) samples);
-    
-    FILE *f = fopen("results.csv", "w");
+    printf("E(byte) = %lf, ", vsota / (double) samples);
+    printf("pi = %lf\n", pi);
 
-    fputs("število;frekvenca\n", f);
-    for (int i = 0; i < 256; i++)
+    printf("Saved info to results.csv\n");
+
+    FILE *csv = fopen("results.csv", "w");
+
+    fputs("število;frekvenca\n", csv);
+    for (int i = 0; i < UINT8_MAX; i++)
     {
-        fprintf(f, "%d;%ld\n", i, frekvenca[i]);
+        fprintf(csv, "%d;%ld\n", i, frekvenca[i]);
     }
 
-    fclose(f);
+    fclose(csv);
 
     return 0;
 }
